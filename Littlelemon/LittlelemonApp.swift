48f2069061aa72bc8a676cf8c52060a8b0eab1f7
SarhAlkhamees <sarhsalkhamees@gmail.com>
2024-05-24 11:18:29 +0300

    Initial Commit

---
+++ Littlelemon/LittlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittlelemonApp.swift
+//  Littlelemon
+//
+//  Created by Sarah Alkhamees on 16/11/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct LittlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
