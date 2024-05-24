c4c9c5f6f6b91043c7afac73b72eece413a6f5d2
SarhAlkhamees <sarhsalkhamees@gmail.com>
2024-05-25 00:04:38 +0300

    Set up the profile page.
    Build the UserProfile page so the user can display their personal information and log out of their account.

--- Littlelemon/Onboarding.swift
+++ Littlelemon/Onboarding.swift
@@ -9,6 +9,7 @@ import SwiftUI
 let kFirstName = "first name key"
 let kLastName = "last name key"
 let kEmail = "email key"
+let kIsLoggedIn = "kIsLoggedIn"
 
 struct Onboarding: View {
     @State var firstName: String = ""
@@ -57,12 +58,20 @@ struct Onboarding: View {
                             UserDefaults.standard.set(firstName, forKey: kFirstName)
                             UserDefaults.standard.set(lastName, forKey: kLastName)
                             UserDefaults.standard.set(email, forKey: kEmail)
+                            UserDefaults.standard.set(true, forKey: kIsLoggedIn)
                             isLoggedIn = true
-                        } else {
-                            showErrorMessage = true
                         }
                     } else {
                         showErrorMessage = true
+                    }
+                }
+                .onAppear{
+                    if UserDefaults.standard.bool(forKey: kIsLoggedIn){
+                        isLoggedIn = true
+                    } else {
+                        firstName = ""
+                        lastName = ""
+                        email = ""
                     }
                 }
             }
